b9599db739fe19a91f720f01dff9be1221e908f3
izabonarowska <ibonarowska@gmail.com>
2023-10-26 14:30:23 +0200

    Feature: Created Plant Service, Plant Controller

--- src/main/java/com/plantapp/plantapp/plant/controller/PlantController.java
+++ src/main/java/com/plantapp/plantapp/plant/controller/PlantController.java
@@ -1,25 +1,79 @@
 package com.plantapp.plantapp.plant.controller;
 
 import com.plantapp.plantapp.plant.model.Plant;
+import com.plantapp.plantapp.plant.service.PlantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/plant")
 public class PlantController {
 
-    @GetMapping("/{plant-name}")
-    public List<Plant> getPlantsByName(@PathVariable("plant-name") String plantName){
-        return null;
+    private final PlantService plantService;
+
+    @Autowired
+    public PlantController(PlantService plantService) {
+        this.plantService = plantService;
     }
 
+    @GetMapping()
+    public ResponseEntity<List<Plant>> getAllPlants(){
+        try{
+            List<Plant> plants = plantService.getAllPlants();
+            return ResponseEntity.ok(plants);
+        } catch(Exception error) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/add")
+    public void addNewPlant(@RequestBody Plant plant){
+       plantService.addNewPlant(
+                plant.getMatureSize(), plant.isToxicity(), plant.isAirPurifying(),
+                plant.getRepotting(), plant.getFertilizer(), plant.getSun(),
+               plant.getWater(), plant.getCareDifficulty(), plant.getBotanicalName(),
+               plant.getCommonName(), plant.getTranslation(), plant.getPlantOverview(),
+               plant.getNativeArea(), plant.getPlantType(), plant.getCareDescription(),
+               plant.getWaterExtended(), plant.getSunExtended(), plant.getTemperature(),
+               plant.getHumidity(), plant.getFertilizerExtended(), plant.getBloomTime(),
+               plant.getRepottingExtended(), plant.getSoilType(), plant.getSoilPh(),
+               plant.getPropagating(), plant.getPestsAndDiseases(), plant.getPruning());
+    }
+
+    @DeleteMapping("/delete/{plant-id}")
+    public ResponseEntity<Optional<Plant>> deletePlantById(@PathVariable("plant-id") int plantId){
+        try{
+            Optional<Plant> plant = plantService.deletePlantById(plantId);
+            return ResponseEntity.ok(plant);
+        } catch(Exception error) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PatchMapping("/{plant-id}")
+
     @GetMapping("/{plant-id}")
-    public Plant getPlantById(@PathVariable("plant-id") int plantId){
-        return null;
+    public ResponseEntity<Optional<Plant>> getPlantById(@PathVariable("plant-id") int plantId){
+        try{
+        Optional<Plant> plant = plantService.getPlantById(plantId);
+        return ResponseEntity.ok(plant);
+        } catch(Exception error) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
-    @GetMapping("/get-plants")
-    public List<Plant> getUserPlants(){
-        return null;
+    @GetMapping("/{plant-name}")
+    public ResponseEntity<List<Plant>> getPlantsByName(@PathVariable("plant-name") String plantName){
+        try{
+            List<Plant> plants = plantService.getPlantsByName(plantName);
+            return ResponseEntity.ok(plants);
+        } catch(Exception error) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
+
 }
--- src/main/java/com/plantapp/plantapp/plant/service/PlantService.java
+++ src/main/java/com/plantapp/plantapp/plant/service/PlantService.java
@@ -1,7 +1,58 @@
 package com.plantapp.plantapp.plant.service;
 
+import com.plantapp.plantapp.plant.model.Plant;
+import com.plantapp.plantapp.plant.repository.PlantRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 
-public class PlantService {
+@Service
+public class PlantService implements IPlantService {
+    private final PlantRepository plantRepository;
 
+    @Autowired
+    public PlantService(PlantRepository plantRepository) {
+        this.plantRepository = plantRepository;
+    }
+
+    @Override
+    public List<Plant> getAllPlants() {
+        return plantRepository.findAll();
+    }
+
+    @Override
+    public Optional<Plant> getPlantById(int plantId) {
+        return plantRepository.findById(plantId);
+    }
+
+    @Override
+    @Transactional
+    public List<Plant> getPlantsByName(String plantName) {
+        String likeExpression = "%" + plantName + "%";
+        return plantRepository.findByBotanicalNameLikeOrCommonNameLike(likeExpression, likeExpression);
+    }
+
+    @Override
+    public Plant addNewPlant(double matureSize, boolean toxicity, boolean airPurifying,
+                                       int repotting, int fertilizer, int sun, int water,
+                                       int careDifficulty, String botanicalName, String commonName,
+                                       String translation, String plantOverview, String nativeArea,
+                                       String plantType, String careDescription, String waterExtended,
+                                       String sunExtended, String temperature, String humidity,
+                                       String fertilizerExtended, String bloomTime, String repottingExtended,
+                                       String soilType, String soilPh, String propagating, String pestsAndDiseases,
+                                       String pruning) {
+        return plantRepository.save(new Plant(matureSize, toxicity, airPurifying, repotting, fertilizer, sun,
+                water, careDifficulty, botanicalName, commonName, translation, plantOverview, nativeArea, plantType, careDescription,
+                waterExtended, sunExtended, temperature, humidity, fertilizerExtended, bloomTime, repottingExtended, soilType, soilPh,
+                propagating, pestsAndDiseases, pruning));
+    }
+
+    @Override
+    public Optional<Plant> deletePlantById(int plantId) {
+        return plantRepository.deleteById(plantId);
+    }
 }
