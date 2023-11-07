850d2b28b1b14c9c5d823053110a296bc1a9d380
adawojcik <adawojcik.i@gmail.com>
2023-11-07 11:16:21 +0100

    Ref:userGameProgress

--- src/main/java/com/plantapp/plantapp/user_game_progress/controller/UserGameProgressController.java
+++ src/main/java/com/plantapp/plantapp/user_game_progress/controller/UserGameProgressController.java
@@ -1,19 +1,34 @@
 package com.plantapp.plantapp.user_game_progress.controller;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import com.plantapp.plantapp.user_game_progress.service.UserGameProgressService;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user-game-progress")
 public class UserGameProgressController {
+    private final UserGameProgressService userGameProgressService;
 
+    public UserGameProgressController(UserGameProgressService userGameProgressService) {
+        this.userGameProgressService = userGameProgressService;
+    }
+    @PostMapping("/post-user-progress")
+    public void postUserExperienceByUserId(@RequestBody int userId, @RequestBody int exp){
+        userGameProgressService.postUserExperienceByUserId(userId, exp);
+    }
     @GetMapping("/get-exp")
-    public int getUserExperienceByUserId(@RequestBody int userId) { return 0; }
+    public int getUserExperienceByUserId(@RequestBody int userId) {
+      return  userGameProgressService.getUserExperienceByUserId(userId);
+    }
 
     @DeleteMapping("/delete-exp")
-    public void removeUserExperienceByUserId(@RequestBody int userId) {}
+    public void removeUserExperienceByUserId(@RequestBody int userId) {
+        userGameProgressService.removeUserExperienceByUserId(userId);
+    }
 
     @PatchMapping("/update-exp")
-    public void updateUserExperienceByUserId(@RequestBody int userId) {}
+    public void updateUserExperienceByUserId(@RequestBody int userId,@RequestBody int exp) {
+        userGameProgressService.updateUserExperienceByUserId(userId, exp);
+    }
 
 }
