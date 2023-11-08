25d1b64fa06b6d0de062306c910c06c14d3e7128
Mateusz Grygier <mgrygierr@gmail.com>
2023-11-08 11:13:08 +0100

    Refactor: Updates to UserController, SecurityConfiguration logout handler added.

--- src/main/java/com/plantapp/plantapp/authentication/AuthenticationService.java
+++ src/main/java/com/plantapp/plantapp/authentication/AuthenticationService.java
@@ -7,7 +7,6 @@ import com.plantapp.plantapp.user_type.UserType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,17 +21,18 @@ public class AuthenticationService {
 
     public AuthenticationResponse register(RegisterRequest request) {
 
-        boolean userExists = userRepository.existsByUserNameOrEmail(request.getUsername(), request.getEmail());
+        boolean userExists = userRepository.existsByUserNameOrEmail(request.getUserName(), request.getEmail());
 
         if (userExists) {
             return null;
         }
 
         var user = new User();
-        user.setUserName(request.getUsername());
+        user.setUserName(request.getUserName());
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setUserType(UserType.USER);
+        user.setActive(true);
         userRepository.save(user);
 
         String jwt = jwtService.generateToken(user);
