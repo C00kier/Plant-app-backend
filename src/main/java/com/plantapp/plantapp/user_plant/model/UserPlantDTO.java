feb0d27dc704e204ef61bf1dc4b02533f75a6583
adawojcik <adawojcik.i@gmail.com>
2023-11-21 15:03:20 +0100

    Fix: UserPlantController, UserPlantService, UserPlant

---
+++ src/main/java/com/plantapp/plantapp/user_plant/model/UserPlantDTO.java
@@ -0,0 +1,11 @@
+package com.plantapp.plantapp.user_plant.model;
+
+
+public record UserPlantDTO(
+        int id,
+        String name,
+        String room,
+        String alias
+
+
+) { }
---
+++ src/main/java/com/plantapp/plantapp/user_plant/service/IUserPlantService.java
@@ -0,0 +1,20 @@
+package com.plantapp.plantapp.user_plant.service;
+
+import com.plantapp.plantapp.user_plant.model.UserPlant;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
+
+public interface IUserPlantService {
+    List<UserPlant> getUserPlantsByUserId(int userId);
+    void addPlantToUserPlantsById(UserPlant userPlant);
+
+    void removePlantFromUserPlantsById(int userPlantId);
+    void updateUserPlantRoomById(int userPlantId, String roomName);
+    void updateUserPlantAliasById(int userPlantId, String alias);
+    void updateLastWateringByUserPlantId(int userPlantId);
+    void updateLastPropagateByUserPlantId(int userPlantId);
+    void updateLastRepotByUserPlantId(int userPlantId);
+    void updateLastFertilizerByUserPlantId(int userPlantId);
+}
